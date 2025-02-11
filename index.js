f7dcb5b56aaa7f76fff3bc67217987639555610e
asmina18 <asmina18@mail.ru>
2025-02-11 11:05:30 +0100

    Env + Router setup

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+import express from "express"
+
+const app = express()
+const port = 4000
+
+app.get("/",(req,res)=>{
+    console.log("Hej verden");
+
+    res.send("Hello world")
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server runs at http://localhost${port}`)
+})
